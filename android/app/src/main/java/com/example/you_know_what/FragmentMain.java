72d8dd8635ec49ffdd0241a917547710a6145a7d
ansuvin <50893537+ansuvin@users.noreply.github.com>
2020-07-27 20:44:16 +0900

    [CREATE] Activity

---
+++ android/app/src/main/java/com/example/you_know_what/FragmentMain.java
@@ -0,0 +1,21 @@
+package com.example.you_know_what;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentMain extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
+        view = inflater.inflate(R.layout.fragment_main,container,false);
+
+        return view;
+    }
+}
---
+++ android/app/src/main/java/com/example/you_know_what/LoadingClass.java
@@ -0,0 +1,42 @@
+package com.example.you_know_what;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+
+public class LoadingClass extends Activity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lodingclass);
+
+        imageView = findViewById(R.id.imageView);
+
+        Animation appear;
+        appear = AnimationUtils.loadAnimation(this, R.anim.translate);
+        imageView.setAnimation(appear);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
