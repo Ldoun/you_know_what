d3bb20383be29c3435c7fad0db18e351ec0a5e75
Ldoun <67096173+Ldoun@users.noreply.github.com>
2020-09-02 16:28:59 +0900

    [Updata[

--- you_know_what_app_server/server.py
+++ you_know_what_app_server/server.py
@@ -9,18 +9,18 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
     from_android=request.data.decode("utf-8")
-    if(from_android[-7:]=='대해 알려 줘'):
+    '''if(from_android[-7:]=='대해 알려 줘'):
         from_android=from_android[:-9]
     else:
         if(from_android[-16:]=='에 대한 새로운 사실 알려줘'):
             print(from_android[:-16])
-        '''else:
+        else:
             if(from_android[-4:]=='알려 줘'):
              from_android=from_android[:-5]'''
         
-        
+    subject=get_NNG(from_android)  
 
         
-    return from_android
+    return subject
 
 app.run(host="0.0.0.0", port=5000, debug=True)
