03529b959f06090734cdd40b057dba417af170b8
Ldoun <67096173+Ldoun@users.noreply.github.com>
2020-09-02 15:19:39 +0900

    [server_INIT]
    INIT

---
+++ you_know_what_app_server/pos.py
@@ -0,0 +1,21 @@
+from konlpy.tag import Kkma
+'''kkma = Kkma()
+x=kkma.pos('고양이에 대해 알려줘')
+print(type(x[0]))
+
+for i in x:
+    if 'NNG' in i:
+        print(i[0])'''
+
+
+
+def get_NNG(sentence):
+    kkma=Kkma()
+    x=kkma.nouns(sentence)
+
+    '''print(x)
+    for i in x:
+        if 'NNG' in i:'''
+    return x[0]
+
+print(get_NNG('강아지에 대해 궁금해졌어!'))
---
+++ you_know_what_app_server/server.py
@@ -0,0 +1,26 @@
+import flask
+from flask import request
+from pos import get_NNG
+
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_request():
+    from_android=request.data.decode("utf-8")
+    if(from_android[-7:]=='대해 알려 줘'):
+        from_android=from_android[:-9]
+    else:
+        if(from_android[-16:]=='에 대한 새로운 사실 알려줘'):
+            print(from_android[:-16])
+        '''else:
+            if(from_android[-4:]=='알려 줘'):
+             from_android=from_android[:-5]'''
+        
+        
+
+        
+    return from_android
+
+app.run(host="0.0.0.0", port=5000, debug=True)
