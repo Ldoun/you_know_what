a6a3c754b1c27d522262636b15d5713a5d8ad2aa
Ldoun <ldoun8260@gmail.com>
2020-09-12 13:38:25 +0000

    from_ubuntu

--- you_know_what_app_server/testserver.py
+++ you_know_what_app_server/testserver.py
@@ -1,12 +1,16 @@
 import flask
-from flask import request
+import ssl
+from flask import request,jsonify
 from pos import get_NNG
 import pymysql
 import random
 from konlpy.tag import Okt
 import json
+from flask_talisman import Talisman
+
 okt=Okt()
 app = flask.Flask(__name__)
+
 stac=pymysql.connect(
      user='stac',
      passwd='',
@@ -14,6 +18,21 @@ stac=pymysql.connect(
      charset='utf8'
      )
 cursor=stac.cursor()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    from_android=request.data.decode("utf-8")
+    user=json.loads(from_android)
+
+    sql="insert into user(password,email) values('"+user['pwd']+"','"+user['email']+"');"
+    sql2="select LAST_INSERT_ID()";
+    print('here')
+    cursor.execute(sql)
+    stac.commit()
+    cursor.execute(sql2)
+    result=cursor.fetchone()
+
+    return str(result[0])
 
 @app.route('/topic', methods=['GET', 'POST'])
 def topic_tip():
@@ -37,4 +56,39 @@ def handle_request():
     user=json.loads(from_android)
     email=user["email"]
 
-app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route('/Topic_tip', methods=['POST'])
+def Topic_tip():
+    req_json = request.json
+    print(req_json)
+    response = {
+        "version": "2.0",
+        "resultCode": "OK",
+        "output": {
+            "Topic": req_json['action']['parameters']['Topic']['value'],
+            "topic_tip": 1,
+            "topic_tip1": 2,
+            "topic_tip2": 3
+            },
+        "directives": []
+        }
+    return jsonify(response)
+@app.route('/random_tip', methods=['POST'])
+def random_tip():
+    req_json = request.json
+    print(req_json)
+    #tips = get_Tips()
+    response = {
+            "version": "2.0",
+            "resultCode": "OK",
+            "output": {
+                "random" : req_json['action']['parameters']['random']['value'],
+                "tip": 1
+                },
+            "directives": []
+            }
+    return jsonify(response)
+if __name__=="__main__":
+    ssl_context=ssl.SSLContext(ssl.PROTOCOL_TLS)
+    ssl_context.load_cert_chain(certfile='/etc/letsencrypt/live/common.stac-know.tk/fullchain.pem',keyfile='/etc/letsencrypt/live/common.stac-know.tk/privkey.pem',password='')
+
+    app.run(host="0.0.0.0",port=5000,debug=True,ssl_context=ssl_context)
