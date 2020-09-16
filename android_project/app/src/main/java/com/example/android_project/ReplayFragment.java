fbfe3cb7df384f1904606ebd388744ddd287ffc8
ansuvin <50893537+ansuvin@users.noreply.github.com>
2020-09-16 18:30:15 +0900

    [UPDATE] error

--- android_project/app/src/main/java/com/example/android_project/ReplayFragment.java
+++ android_project/app/src/main/java/com/example/android_project/ReplayFragment.java
@@ -1,18 +1,45 @@
 package com.example.android_project;
 
 import android.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ReplayFragment extends Fragment {
     View view;
+    String str;
+    String[] array;
+
+    String ipv4address = "common.stac-know.tk";
+    String portnum = "5000";
+    String postUrl = "https://" + ipv4address + ":" + portnum + "/";
+    request request;
+
+    String strEmail;
 
     private ListView listView;
     private ListViewAdapter adapter;
@@ -27,6 +54,121 @@ public class ReplayFragment extends Fragment {
         listView= view.findViewById(R.id.listView);
         listView.setAdapter(adapter);
 
+        request = new request(postUrl);
+        request.status=4;
+        try {
+            request.revivew_requests(strEmail);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return view;
+    }
+    void tts(String str){
+
+    }
+
+    public class request{
+        String Usernum;
+        RequestBody requestBody;
+        MediaType mediaType;
+        request(String URL){
+            this.URL=URL;
+            System.out.println(URL);
+        }
+        int status;
+        String URL;
+        /*String result;
+        String getter(){
+            return result;
+        }*/
+        public void requests(String msg) throws JSONException {
+
+            JSONObject json=new JSONObject();
+            json.put("email","1234@gmail.com");
+            json.put("topic",msg);
+            json.put("usernum",Usernum);
+            mediaType = MediaType.parse("application/json");
+            requestBody = RequestBody.create(json.toString(), mediaType);
+            System.out.println(URL);
+            postRequest(requestBody,this.URL+"topic");
+            /*if(result!=null){
+                tts.speak(result,TextToSpeech.QUEUE_FLUSH, null);
+            }*/
+        }
+        void revivew_requests(String id) throws JSONException {
+            System.out.println("함");
+            JSONObject json=new JSONObject();
+            json.put("id","27");
+            mediaType = MediaType.parse("application/json");
+            requestBody = RequestBody.create(json.toString(), mediaType);
+            postRequest(requestBody,this.URL+"review");
+            /*if(result!=null){
+                tts.speak(result, TextToSpeech.QUEUE_FLUSH, null);
+            }*/
+        }
+
+        public void requests(String password, String email) throws JSONException {
+            JSONObject json=new JSONObject();
+            json.put("pwd",password);
+            json.put("email",email);
+
+            mediaType = MediaType.parse("application/json");
+            requestBody = RequestBody.create(json.toString(), mediaType);
+            postRequest(requestBody,this.URL+"register");
+        }
+
+        private void postRequest(RequestBody requestBody, String url) throws JSONException {
+            //RequestBody requestBody = buildRequestBody(message);
+            System.out.println(url);
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request request = new Request
+                    .Builder()
+                    .post(requestBody)
+                    .url(url)
+                    .build();
+
+            okHttpClient.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(final Call call, final IOException e) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @RequiresApi(api = Build.VERSION_CODES.M)
+                        @Override
+                        public void run() {
+                            Toast.makeText(ReplayFragment.this.getContext(), "Something went wrong:" + " " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            call.cancel();
+                        }
+                    });
+
+                }
+
+                @Override
+                public void onResponse(Call call, final Response response) throws IOException {
+
+                    if(status==0){
+                        tts(response.body().string());
+                    }else if(status==4){
+                        review(response.body().string());
+                    }
+
+                }
+            });
+
+        }
+
+    }
+
+    void review (String string){
+        //array = string.split(":");
+
+        str = "안수빈,안수빈,안녕,하이,메롱";
+        array = str.split(",");
+
+//출력
+        for(int i=0;i<array.length;i++) {
+            adapter.addItem(array[i]);
+        }
+
         adapter.addItem("트위터의 드립 문화를 일컫는 유행어. 글자 그대로 아무 말이나 마구 하는 모양새를 일컫는 말로, 아무렇게나 말을 내뱉기 편한 트위터의 특성을 그대로 표현하고 있다." +
                 "아래의 코너가 방송된 이후로 트위터 내에서는");
         adapter.addItem("동 생성'의 경우국어 인식 정확도는 최악이라서 이 기능을 사");
@@ -37,7 +179,5 @@ public class ReplayFragment extends Fragment {
                 "줄자에는 한쪽에 cm가 표기되어 있고, 다른 쪽에는 inch가 표기되어 있는데 경우에 따라 cm로 재다가 inch로 잰다면 어떻게 될까? 형평의 원리에 어긋나기  때문에 난리가 날 것이다. 내가 볼 때 지금이 꼭 그렇다. 국회, 언론, 검찰이 내부자가 되어 이중잣대를 들이밀며 세상을 재단하고 있는 것이다. 국회의원이 앞장서면 언론은 선동하고 검찰이 행동대장 역할을 하고 있는 꼴이다. 평정심을 잃은 정도가 아니라 제정신이 아닌 것 같다.");
 
         adapter.notifyDataSetChanged();
-
-        return view;
     }
 }
--- android_project/app/src/main/java/com/example/android_project/SignupActivity.java
+++ android_project/app/src/main/java/com/example/android_project/SignupActivity.java
@@ -2,9 +2,13 @@ package com.example.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -12,12 +16,27 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Locale;
 import java.util.regex.Pattern;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
 
-    String ipv4address = "54.210.63.142";
+    String ipv4address = "common.stac-know.tk";
     String portnum = "5000";
+
     String postUrl = "http://" + ipv4address + ":" + portnum + "/";
 
     EditText editEmail, editPW;
@@ -27,6 +46,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     String strEmail, strPW;
     boolean bEmail =false, bPW=false, bChecked=false;
     int checked=0;
+    request request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,34 +61,44 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         textPW = findViewById(R.id.textPW);
 
         btnSignup.setOnClickListener(this);
+        btnCheck.setOnClickListener(this);
+
+        request=new request(postUrl);
+        request.status=1;
+
     }
 
     @Override
     public void onClick(View view) {
+        strEmail = editEmail.getText().toString();
+        strPW = editPW.getText().toString();
         switch (view.getId()) {
             case R.id.btnSignup:
                 if (editEmail.getText().length() > 0 && editPW.getText().length() > 0) {
-                    strEmail = editEmail.getText().toString();
-                    strPW = editPW.getText().toString();
                     if(checked == 0){
                         textId.setText("중복 체크를 해주세요");
                         textId.setTextColor(Color.RED);
                     }
-                    if (!Pattern.matches("^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-zA-Z]).{8,20}$", strPW)) {
-                        textPW.setText("비밀번호 형식을 지켜주세요");
-                        textPW.setTextColor(Color.RED);
-                    }
-                    if(Pattern.matches("^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-zA-Z]).{8,20}$", strPW)){
+                    if(Pattern.matches("^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-zA-Z]).{8,12}$", strPW)){
                         textPW.setText("비밀번호 형식이 맞습니다.");
                         textPW.setTextColor(Color.LTGRAY);
                         bPW = true;
+                    } else {
+                        textPW.setText("비밀번호 형식이 아닙니다. 특수문자 포함 숫자");
                     }
                     if(!bEmail){
                         textId.setText("Email 형식이 아닙니다.");
                         textId.setTextColor(Color.RED);
                     }
                     if(bEmail && bChecked && bPW){
-                        postRequests(strEmail, strPW,postUrl);
+                        try {
+                            request.requests(strEmail, strPW);
+                            Toast.makeText(this, "통과", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
                 else {
@@ -76,10 +106,8 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
                 }
                 break;
             case R.id.btnCheck:
-                if (editEmail.getText().length() > 0) {
-                    strEmail = editEmail.getText().toString();
-                    strPW = editPW.getText().toString();
-                    if (!Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
+                bEmail = true;
+                    if (!android.util.Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
                         textId.setText("Email 형식이 아닙니다.");
                         textId.setTextColor(Color.RED);
                     }
@@ -87,32 +115,138 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
                         textId.setTextColor(Color.LTGRAY);
                         textId.setText("Email 형식이 맞습니다.");
                         bEmail = true;
-                        postRequestEmailCheck(strEmail, postUrl);
+                        try {
+                            request.requests(strEmail);
+                            checked =1;
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        if(checked ==0){
+                            textId.setText("이미 존재하는 Email입니다.");
+                            textId.setTextColor(Color.RED);
+                        }
+                        else{
+                            textId.setTextColor(Color.LTGRAY);
+                            textId.setText("중복 확인 완료");
+                            bChecked = true;
+                            btnCheck.setEnabled(false);
+                        }
                     }
-                }
-                else {
-                    Toast.makeText(this, "email나 Password는 비워둘 수 없습니다.", Toast.LENGTH_SHORT).show();
-                }
                 break;
         }
     }
 
-    private void postRequests(String strEmail, String strPW, String URL){
+    void tts(String str){
 
     }
 
-    private void postRequestEmailCheck (String strEmail, String URL){
+    void registerer(String str){
+        SharedPreferences sharedPreferences = getSharedPreferences("sFile",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        checked = 1;
-        if(checked ==0){
-            textId.setText("이미 존재하는 Email입니다.");
-            textId.setTextColor(Color.RED);
+        editor.putString("text",str);
+        editor.commit();
+
+        Log.d("registerer_true",str);
+
+    }
+    public class request{
+        String Usernum;
+        RequestBody requestBody;
+        MediaType mediaType;
+        request(String URL){
+            this.URL=URL;
+            System.out.println(URL);
         }
-        else{
-            textId.setTextColor(Color.LTGRAY);
-            textId.setText("중복 확인 완료");
-            bChecked = true;
-            btnCheck.setEnabled(false);
+        int status;
+        String URL;
+        /*String result;
+        String getter(){
+            return result;
+        }*/
+        public void requests(String msg) throws JSONException {
+
+            JSONObject json=new JSONObject();
+            json.put("email","1234@gmail.com");
+            json.put("topic",msg);
+            json.put("usernum",Usernum);
+            mediaType = MediaType.parse("application/json");
+            requestBody = RequestBody.create(json.toString(), mediaType);
+            System.out.println(URL);
+            postRequest(requestBody,this.URL+"topic");
+            /*if(result!=null){
+                tts.speak(result,TextToSpeech.QUEUE_FLUSH, null);
+            }*/
         }
+        void revivew_requests(String email) throws JSONException {
+            JSONObject json=new JSONObject();
+            json.put("email","1234@gmail.com");
+            mediaType = MediaType.parse("application/json");
+            requestBody = RequestBody.create(json.toString(), mediaType);
+            postRequest(requestBody,this.URL+"review");
+            /*if(result!=null){
+                tts.speak(result,TextToSpeech.QUEUE_FLUSH, null);
+            }*/
+        }
+
+        public void requests(String password, String email) throws JSONException {
+            JSONObject json=new JSONObject();
+            json.put("pwd",password);
+            json.put("email",email);
+
+            mediaType = MediaType.parse("application/json");
+            requestBody = RequestBody.create(json.toString(), mediaType);
+            postRequest(requestBody,this.URL+"register");
+        }
+
+        private void postRequest(RequestBody requestBody, String url) throws JSONException {
+            //RequestBody requestBody = buildRequestBody(message);
+            System.out.println(url);
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request request = new Request
+                    .Builder()
+                    .post(requestBody)
+                    .url(url)
+                    .build();
+
+            okHttpClient.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(final Call call, final IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(SignupActivity.this, "Something went wrong:" + " " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            call.cancel();
+                        }
+                    });
+
+                }
+
+                @Override
+                public void onResponse(Call call, final Response response) throws IOException {
+
+                    if(status==0){
+                        tts(response.body().string());
+                    }
+                    else if(status==1){
+                        registerer(response.body().string());
+                    }else if(status==4){
+                        review(response.body().string());
+                    }
+
+                }
+            });
+
+        }
+
+    }
+
+    void review (String str){
+
     }
 }
+
+
+
+
+
